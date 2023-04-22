156cd13f3bf9dded60519c4f12bca2a5556a4250
Hillario <hillcheswiztyphoon@gmail.com>
2023-04-22 14:52:18 +0300

    Added post api,formgroup and formcontrols

--- client/src/app/addtrade/addtrade.component.ts
+++ client/src/app/addtrade/addtrade.component.ts
@@ -1,5 +1,7 @@
 import { Component} from '@angular/core';
-import {FormControl, Validators} from '@angular/forms';
+import {FormControl, Validators, FormGroup} from '@angular/forms';
+import { DataserviceService } from '../dataservice.service';
+
 
 
 @Component({
@@ -8,6 +10,7 @@ import {FormControl, Validators} from '@angular/forms';
   styleUrls: ['./addtrade.component.scss']
 })
 export class AddtradeComponent{
+  myForm: FormGroup|any;
   numFormControl = new FormControl('', [Validators.required]);
   myStake: number | any;
   myPayout: number | any;
@@ -17,5 +20,38 @@ export class AddtradeComponent{
   myProfit: number | any;
   myStart: string | any;
   myEnd: string | any;
+
+  constructor(private postService: DataserviceService) {}
+
+  ngOnInit()
+  {
+    this.myForm = new FormGroup({
+      myStake: new FormControl(''),
+      myPayout: new FormControl(''),
+      myRuns: new FormControl(''),
+      myLost: new FormControl(''),
+      myWon: new FormControl(''),
+      myProfit: new FormControl(''),
+      myStart: new FormControl(''),
+      myEnd: new FormControl('')
+    });
+  }
+
+
+
+
+  submitForm() {
+    const formData = this.myForm.value;
+    this.postService.postFormData(formData)
+      .subscribe(
+        res => {
+          console.log(res);
+        },
+        err => {
+          console.log(err);
+        }
+      );
+  }
+
 
 }
--- client/src/app/dataservice.service.ts
+++ client/src/app/dataservice.service.ts
@@ -7,11 +7,16 @@ import { HttpClient } from '@angular/common/http';
 export class DataserviceService {
 
   private dataUrl = 'http://localhost:5000/api/get';
+  private API_URL = 'http://localhost:5000';
 
   constructor(private http: HttpClient) { }
 
   getData() {
     return this.http.get(this.dataUrl);
+  }
+
+  postFormData(formData: any) {
+    return this.http.post(`${this.API_URL}/api/post`, formData);
   }
 
 }
